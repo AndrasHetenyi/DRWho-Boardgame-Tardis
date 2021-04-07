63c0cae3e86753237810b22721cc99fb719763cb
AndrasHetenyi <57223199+AndrasHetenyi@users.noreply.github.com>
2021-04-07 22:18:04 +0100

    Add files via upload

---
+++ DrWho/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace DrWho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        string[] lines = Properties.Resources.voices_text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Doctor Who", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(569, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Doctor Who", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(444, 217);
+            this.label2.MaximumSize = new System.Drawing.Size(300, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "click on the TARDIS";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.BackgroundImage = global::DrWho.Properties.Resources.tardis_transparent_png;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(398, 426);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(521, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Doctor Who Interactive Electronic Board Game - TARDIS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DrWho/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+
+namespace DrWho
+{
+    public partial class Form1 : Form
+    {
+    public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int pick = random.Next(1, 13);
+            this.label3.Text = "";
+            this.label1.Text = pick.ToString();
+
+            this.label2.Text = lines[pick-1];
+            Stream str;
+            switch (pick)
+            {
+                case 1:
+                    str = Properties.Resources.sound1;
+                    break;
+                case 2:
+                    str = Properties.Resources.sound2;
+                    break;
+                case 3:
+                    str = Properties.Resources.sound3;
+                    this.label3.Text = "Sound missing";
+                    break;
+                case 4:
+                    str = Properties.Resources.sound4;
+                    break;
+                case 5:
+                    str = Properties.Resources.sound5;
+                    break;
+                case 6:
+                    str = Properties.Resources.sound6;
+                    break;
+                case 7:
+                    str = Properties.Resources.sound7;
+                    break;
+                case 8:
+                    str = Properties.Resources.sound8;
+                    break;
+                case 9:
+                    str = Properties.Resources.sound9;
+                    break;
+                case 10:
+                    str = Properties.Resources.sound10;
+                    break;
+                case 11:
+                    str = Properties.Resources.sound11;
+                    break;
+                case 12:
+                    str = Properties.Resources.sound12;
+                    break;
+                default:
+                    str = Properties.Resources.sound1;
+                    break;
+            }
+            SoundPlayer snd = new SoundPlayer(str);
+            snd.Play();
+           
+        }
+    }
+}
